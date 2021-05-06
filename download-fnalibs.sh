8f288a52e7fa4e457d38544d200e1063fd22d450
Chad Yates <cyates@dynfxdigital.com>
2021-05-05 18:11:32 -0700

    Add script to automatically download and uncompress the latest fnalibs package.

---
+++ download-fnalibs.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Checks if git is installed
+function checkGit()
+{
+    git --version > /dev/null 2>&1
+    if [ ! $? -eq 0 ]; then
+        echo >&2 "ERROR: Git is not installed. Please install git to download FNA."
+        exit 1
+    fi
+}
+
+# Downloads and extracts prepackaged archive of native libraries ("fnalibs")
+function getLibs()
+{
+    # Downloading
+    echo "Downloading latest fnalibs..."
+    curl http://fna.flibitijibibo.com/archive/fnalibs.tar.bz2 > "$MY_DIR/fnalibs.tar.bz2"
+    if [ $? -eq 0 ]; then
+        echo "Finished downloading!"
+    else
+        >&2 echo "ERROR: Unable to download successfully."
+        exit 1
+    fi
+
+    # Decompressing
+    echo "Decompressing fnalibs..."
+    #mkdir -p $MY_DIR
+    tar xjC $MY_DIR -f $MY_DIR/fnalibs.tar.bz2
+    if [ $? -eq 0 ]; then
+        echo "Finished decompressing!"
+        echo ""
+        rm $MY_DIR/fnalibs.tar.bz2
+    else
+        >&2 echo "ERROR: Unable to decompress successfully."
+        exit 1
+    fi
+}
+
+# Get the directory of this script
+MY_DIR=$(dirname "$BASH_SOURCE")
+
+
+# gather input
+
+# FNALibs
+if [ ! -d "$MY_DIR/fnalibs" ]; then
+    read -p "Download fnalibs (y/n)? " shouldDownloadLibs
+else 
+    read -p "Redownload fnalibs (y/n)? " shouldDownloadLibs
+fi
+
+
+# act on the input
+
+# FNALIBS
+if [[ $shouldDownloadLibs =~ ^[Yy]$ ]]; then
+    getLibs
+fi
