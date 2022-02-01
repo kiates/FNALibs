e0e2dca0af1f8066c18be80c3379668260a7006f
Chad Yates <cyates@dynfxdigital.com>
2022-01-31 16:51:22 -0800

    Make script to update-ios-tvos-libs using a remote mac and then copying them over.

--- update-ios-tvos-libs.sh
+++ update-ios-tvos-libs.sh
@@ -1,1 +1,3 @@
+#!/bin/bash -x
+
 ssh cyates@fruity 'source /etc/profile; bash -s' < /mnt/c/Users/cyates/gitwork/Solaroids/ThirdParty/FNALibs/update-ios-tvos-libs-remote.sh && scp -r cyates@fruity:/Users/cyates/gitwork/fnalibs-ios-builder/release/* /mnt/c/Users/cyates/gitwork/Solaroids/ThirdParty/FNALibs/
